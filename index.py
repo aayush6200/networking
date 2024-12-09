5d0fb215dde4d9408d3d5cebb0f90b1d88256f26
Aayush Thapaliya <106351327+aayush6200@users.noreply.github.com>
2024-12-08 22:30:46 -0600

    Added index.py

---
+++ index.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes by default
+
+# Public endpoint
+
+
+@app.route('/helloPrivate', methods=['GET'])
+def say_hello_private():
+    return jsonify({"message": "Hello From a private World!"})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
